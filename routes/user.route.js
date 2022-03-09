c318747c3449b6add36ec443c36cabb0c20e54b0
achrefgallah <achrefgallah@gmail.com>
2022-03-09 19:18:50 +0100

    update auth controller

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,8 +4,8 @@ const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
 
 router.post("/register", authController.signUp);
-// router.post("/login", authController.signIn);
-// router.get("/logout", authController.logout);
+router.post("/login", authController.signIn);
+router.get("/logout", authController.logout);
 
 
 router.get("/",userController.getAllUsers)

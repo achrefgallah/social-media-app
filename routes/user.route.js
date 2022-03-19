f114ebba4881fd20223fbd16133b5549a6ca39ab
achrefgallah <achrefgallah@gmail.com>
2022-03-19 13:15:45 +0100

    upload controller

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,9 @@ const express= require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
+const uploadController = require("../controllers/upload.controller");
+const multer = require("multer");
+const upload = multer();
 
 router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
@@ -14,5 +17,7 @@ router.put("/:id",userController.updateUser)
 router.delete('/:id',userController.deleteUser)
 router.patch('/follow/:id',userController.follow)
 router.patch('/unfollow/:id',userController.unFollow)
+
+router.post('/upload',upload.single('file') ,uploadController.uploadProfil)
 
 module.exports = router;
